2b25ec0507ca231f08243a7b079f9da6bc4b639a
igibek <koishybayev.igibek@gmail.com>
2021-01-10 11:50:46 -0500

    small changes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const cache = require('@actions/cache');
+const fs = require('fs');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -7,9 +9,11 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  console.log(process.cwd());
 } catch (error) {
   core.setFailed(error.message);
 }
