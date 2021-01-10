99fe97235d6e3dc65fc509ccca32c17abf15e581
igibek <koishybayev.igibek@gmail.com>
2021-01-10 13:18:05 -0500

    reading the contents

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
   console.log(process.cwd());
+  let contents = fs.readdirSync(process.cwd());
+  let contents2 = fs.readdirSync('./');
+  console.log('contents: ', contents);
+  console.log('contents: ', contents2);
 } catch (error) {
   core.setFailed(error.message);
 }
