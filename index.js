7fab0f745e63ef973de80e42df2e6f9432a873a9
igibek <koishybayev.igibek@gmail.com>
2021-01-25 08:13:20 -0500

    updated the repo

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { Octokit } = require('@octokit/rest');
-const secret = core.getInput('secret');
+const { createActionAuth } = require("@octokit/auth-action");
+const secret = core.getInput('GITHUB_TOKEN');
 const octokit = new Octokit({
   auth: secret
 });
 try {
+
   octokit.issues.create({
     owner: "kapk8s",
     repo: "gaction-test",
@@ -15,9 +17,16 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  console.log('Octokit:', octokit);
+  // const payload = JSON.stringify(github.context, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+  // console.log('Octokit:', octokit);
+
+  octokit.repos.update({
+    owner: "kapk8s",
+    repo: "gaction-test-module",
+    private: false
+  });
+  
 } catch (error) {
   core.setFailed(error.message);
 }
