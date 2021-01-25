cce7dbe91befd3363ee0605fbb4d618237d1c8bf
igibek <koishybayev.igibek@gmail.com>
2021-01-24 20:55:53 -0500

    retrieving the github token

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const Octokit = require('@octokit/rest');
+const octokit = new Octokit({
+  auth: core.getInput('secret')
+});
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const secret = core.getInput('secret');
+  console.log(`Secret: ${secret}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  console.log('Octokit:', octokit);
 } catch (error) {
   core.setFailed(error.message);
 }
