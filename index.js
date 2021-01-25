66ac6aaf68d5328b5b91c7feaa29a68835bc0c63
igibek <koishybayev.igibek@gmail.com>
2021-01-24 21:30:41 -0500

    github.context analysis

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ const secret = core.getInput('secret');
 const octokit = new Octokit({
   auth: secret
 });
-
 try {
   octokit.issues.create({
     owner: "kapk8s",
     repo: "gaction-test",
     title: "Issue created by action"
   });  
-  console.log(`Secret: ${secret}!`);
+  console.log(`Secret: ${secret.split()}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context, undefined, 2)
   console.log(`The event payload: ${payload}`);
   console.log('Octokit:', octokit);
 } catch (error) {
